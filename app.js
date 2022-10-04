a5079fe3067f9cd9c855e7a423bdff39f578c9b5
austinplambeck <90013723+austinplambeck@users.noreply.github.com>
2022-10-04 13:11:24 -0700

    comm2 - day 1 (homepage done?)

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 // SELECTORS
+const volumeButton = document.getElementById('volume-button');
+const music = document.getElementById('hourglass-mp3');
 
 // EVENT LISTENERS
 
 // FUNCTIONS
+
+// Scrolls to the top of the page
 function scrollUp() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+function toggleMusic() {
+    volumeButton.style.display = 'block';
+    music.play();
+}
+
+function pausePlay() {
+    if(music.paused) {
+        music.play();
+        volumeButton.innerHTML = '<i class="fa-sharp fa-solid fa-volume-high"></i>';
+    } else {
+        music.pause();
+        volumeButton.innerHTML = '<i class="fa-sharp fa-solid fa-volume-xmark"></i>';
+    }
 }
