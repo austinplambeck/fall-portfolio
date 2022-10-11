ce1442307fed4c6de16bd36b09a829bc9e453535
austinplambeck <90013723+austinplambeck@users.noreply.github.com>
2022-10-11 16:07:43 -0700

    comm10 - dark mode v1

--- app.js
+++ app.js
@@ -36,9 +36,43 @@ function pausePlay() {
 }
 
 // DARK MODE
-const wholePage = document.getElementById('page-container');
+// check for saved 'darkMode' in localStorage
+let darkMode = localStorage.getItem('darkMode'); 
 
-function darkToggle() {
-    wholePage.classList.toggle('dark');
-    document.getElementsByClassName('header-format').classList.toggle('dark');
+const darkModeToggle = document.querySelector('#dark-mode-toggle');
+
+const enableDarkMode = () => {
+  // 1. Add the class to the body
+  document.body.classList.add('darkmode');
+  // 2. Update darkMode in localStorage
+  localStorage.setItem('darkMode', 'enabled');
+  // 3. Change icon to moon
+  
 }
+
+const disableDarkMode = () => {
+  // 1. Remove the class from the body
+  document.body.classList.remove('darkmode');
+  // 2. Update darkMode in localStorage 
+  localStorage.setItem('darkMode', null);
+}
+ 
+// If the user already visited and enabled darkMode
+// start things off with it on
+if (darkMode === 'enabled') {
+  enableDarkMode();
+}
+
+// When someone clicks the button
+darkModeToggle.addEventListener('click', () => {
+  // get their darkMode setting
+  darkMode = localStorage.getItem('darkMode'); 
+  
+  // if it not current enabled, enable it
+  if (darkMode !== 'enabled') {
+    enableDarkMode();
+  // if it has been enabled, turn it off  
+  } else {  
+    disableDarkMode(); 
+  }
+});
